f8e246692632f651965cf2adae6e5af18b3b1a09
remzr <110097561+remzr@users.noreply.github.com>
2025-11-05 22:14:18 +0100

    Add CSS and image module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 // index.js
+import "./styles.css";
 import { greeting } from "./greeting.js";
+
+import catImage from "./img/profile-picture.jpg";
+   
+const image = document.createElement("img");
+image.src = catImage;
+   
+document.body.img.appendChild(image);
 
 console.log(greeting);
--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,21 @@ module.exports = {
             template: "./src/template.html",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            }
+        ],
+    },
     mode: "development"
 };
