940f2e2040b52f91926c5438448697c67c1d4ab7
remzr <110097561+remzr@users.noreply.github.com>
2025-11-08 15:20:00 +0100

    Create global event distribution and onclick function to navigate (not working state)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 // index.js
 import "./styles.css";
 import { loadHome } from "./home.js";
+import { loadMenu } from "./menu.js";
+
+//Reapplies event listeners to nav buttons
+function eventDist() {
+    let elements = document.querySelectorAll(".nav-button");
+    
+    elements.forEach((element) => {
+        element.addEventListener("click", navClick(element.id));
+        console.log(element.onClick);
+    });
+};
+
+//Clickevent for nav buttons -> reloads site with selected content
+function navClick(target) {
+    if (target == "navButtonHome") {
+        loadHome();
+        eventDist();
+    } else if (target == "navButtonMenu") {
+        loadMenu();
+        eventDist(); }
+    /* } else {
+        loadAbout();
+        eventDist();
+    } */
+};
 
 loadHome();
+eventDist();
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,5 @@
 //Call to load home content
-export function loadHome() {
+export function loadMenu() {
     //Get header and main
     const headerContent = document.getElementById("header");
     const bodyContent = document.getElementById("content");
@@ -9,16 +9,13 @@ export function loadHome() {
     bodyContent.replaceWith(content);
 }
 
-//Import image
-import homeImage from "./img/dessert-1.jpg";
-
 //Create header
 const nav = document.createElement("header");
 
 nav.innerHTML = `
         <nav>
-            <button id="navButtonHome" class="nav-button nav-active">Home</button>
-            <button id="navButtonMenu" class="nav-button">Menu</button>
+            <button id="navButtonHome" class="nav-button">Home</button>
+            <button id="navButtonMenu" class="nav-button nav-active">Menu</button>
             <button id="navButtonAbout" class="nav-button">About</button>
         </nav>
     `;
@@ -28,13 +25,16 @@ const content = document.createElement("div");
 content.id = "content";
 
 content.innerHTML = `
-        <aside>
-            <img src="${homeImage}" width="400px">
-            <p>We have flans.</p>
-        </aside>
         <main>
-            <h1>THE HOUSE OF SWEETS</h1>
-            <p><strong>Dessert lovers assemble.</strong></p>
-            <p>A plate of melting sugar to share for your friends or munch all by yourself. We don't judge.</p>
+            <h1>OUR MENU</h1>
+            <p><strong>Flan</strong></p>
+            <p>Baked custard dessert topped with a layer of caramel sauce</p>
+            <br>
+            <p><strong>Nutella Cake</strong></p>
+            <p>Cake, but made with Nutella.</p>
+            <br>
+            <p><strong>Fondue</strong></p>
+            <p>Not a fondue, but its still on the menu. Don't ask why.</p>
+            <br>        
         </main>
 `;
